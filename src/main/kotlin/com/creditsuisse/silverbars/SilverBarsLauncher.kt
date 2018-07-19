c4a163853c2cd16a3dd0c1d4a8dd6ae45b1af589
Eugene Gerogiokas <eugene.gerogiokas@springer.com>
2018-07-19 19:33:05 +0100

    minor refactor to use "reduce" instead of "sumByDouble". this is in case it was necessary to change to "BigDecimal" or another type.

--- src/main/kotlin/com/creditsuisse/silverbars/SilverBarsLauncher.kt
+++ src/main/kotlin/com/creditsuisse/silverbars/SilverBarsLauncher.kt
@@ -2,11 +2,6 @@ package com.creditsuisse.silverbars
 
 import java.util.*
 
-
-fun main(args: Array<String>) {
-    val liveOrderBoard = LiveOrderBoard()
-}
-
 data class OrderSummary(val orderQuantity: OrderQuantity, val pricePerKilo: PricePerKilo, val listOfOrderIds: List<OrderId>)
 
 data class Order(val userId: UserId, val orderQuantity: OrderQuantity, val pricePerKilo: PricePerKilo, val orderType: OrderType, val id: OrderId = OrderId(UUID.randomUUID().toString()))
@@ -27,15 +22,19 @@ class LiveOrderBoard {
     fun getSellSummaryInformation(): List<OrderSummary> {
         return orders.filter { it.orderType == OrderType.SELL }
                 .groupBy { it.pricePerKilo }
-                .map { OrderSummary(OrderQuantity(it.value.sumByDouble({ it.orderQuantity.amount })), it.key, it.value.map { it.id }) }
+                .map { entry -> OrderSummary(OrderQuantity(reduceOrderQuantity(entry)), entry.key, entry.value.map { it.id }) }
                 .sortedBy { it.pricePerKilo.price }
     }
 
     fun getBuySummaryInformation(): List<OrderSummary> {
         return orders.filter { it.orderType == OrderType.BUY }
                 .groupBy { it.pricePerKilo }
-                .map { OrderSummary(OrderQuantity(it.value.sumByDouble({ it.orderQuantity.amount })), it.key, it.value.map { it.id }) }
-                .sortedByDescending { it.pricePerKilo.price }    }
+                .map { entry -> OrderSummary(OrderQuantity(reduceOrderQuantity(entry)), entry.key, entry.value.map { it.id }) }
+                .sortedByDescending { it.pricePerKilo.price }
+    }
+
+    private fun reduceOrderQuantity(entry: Map.Entry<PricePerKilo, List<Order>>) =
+            entry.value.map { it.orderQuantity.amount }.reduce { acc, amount -> amount + acc }
 
 }
 
